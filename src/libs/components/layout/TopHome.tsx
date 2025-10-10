992c259befddb9c86add41d69040f543043e5e58
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-10-10 14:24:48 +0900

    feat: develop rught side of the TopHome component

--- src/libs/components/layout/TopHome.tsx
+++ src/libs/components/layout/TopHome.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Stack, Typography } from "@mui/material";
 import LuggageIcon from "@mui/icons-material/Luggage";
 import CameraIcon from "@mui/icons-material/Camera";
 import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import Image from "next/image";
 
 const TopHome = () => {
   return (
@@ -35,13 +36,13 @@ const TopHome = () => {
             width: "167px",
             height: "41px",
             color: "white",
-            mt: 4,
+            mt: 7,
             borderRadius: "7px",
           }}
         >
           Show More
         </Button>
-        <Stack sx={{ mt: 5 }} direction={"row"} spacing={5}>
+        <Stack sx={{ mt: 10 }} direction={"row"} spacing={5}>
           <Stack>
             <LuggageIcon />
             <Stack color={"text.disabled"} flexDirection={"row"} gap={"3px"}>
@@ -90,6 +91,37 @@ const TopHome = () => {
           </Stack>
         </Stack>
       </Stack>
+      <Stack
+        className="right-side"
+        sx={{
+          width: "520px",
+          height: "410px",
+          borderRadius: "100px 15px 15px 15px",
+          overflow: "hidden",
+          position: "relative",
+        }}
+      >
+        <Image
+          src="/img/lanka-stay.jpg"
+          alt="user-image"
+          width={520}
+          height={510}
+          style={{ objectFit: "cover" }}
+        />
+      </Stack>
+      <Box
+        sx={{
+          border: `1px solid`,
+          borderColor: "text.disabled",
+          position: "absolute",
+          width: "520px",
+          height: "410px",
+          top: 50,
+          right: 140,
+          zIndex: -1,
+          borderRadius: "15px",
+        }}
+      ></Box>
       <Stack className="right-side"></Stack>
     </Stack>
   );
