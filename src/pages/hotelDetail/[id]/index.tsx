91699a005bb0352a5ee6e616893136ce8eb69330
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-10-13 08:52:48 +0900

    feat: develop AboutThisHotel sectional compinent

--- src/pages/hotelDetail/[id]/index.tsx
+++ src/pages/hotelDetail/[id]/index.tsx
@@ -1,3 +1,4 @@
+import AboutThisHotel from "@/src/libs/components/HotelDetail.tsx/AboutThisHotel";
 import HotelInfoSection from "@/src/libs/components/HotelDetail.tsx/HotelInfoSection";
 import PropertyOverview from "@/src/libs/components/HotelDetail.tsx/PropertyOverview";
 import withLayoutSecondary from "@/src/libs/components/layout/LayoutSecondary";
@@ -9,6 +10,7 @@ const HotelDetail = () => {
     <Stack>
       <PropertyOverview />
       <HotelInfoSection />
+      <AboutThisHotel />
     </Stack>
   );
 };
