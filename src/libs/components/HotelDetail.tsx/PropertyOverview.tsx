51f7b37a995912235b889dd15d7b5ddee96262e8
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-10-13 10:11:20 +0900

    feat: develop Share logic for Hote;Detail page

--- src/libs/components/HotelDetail.tsx/PropertyOverview.tsx
+++ src/libs/components/HotelDetail.tsx/PropertyOverview.tsx
@@ -7,6 +7,11 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import WePriceMatchDialog from "./WePriceMatchDialog";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 const PropertyOverview = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -17,8 +22,37 @@ const PropertyOverview = () => {
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const handleShare = async () => {
+    const url = window.location.href; // current page URL
+
+    try {
+      await navigator.clipboard.writeText(url);
+      alert("✅ Link copied to clipboard!");
+      setAnchorShareEl(null);
+    } catch (err) {
+      console.error("Failed to copy:", err);
+    }
+  };
   const isBeachFront = true;
   const like = true;
+
+  const [anchorShareEl, setAnchorShareEl] = useState<null | HTMLElement>(null);
+  const shareOpen = Boolean(anchorShareEl);
+  const url = typeof window !== "undefined" ? window.location.href : "";
+
+  const handleShareClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorShareEl(event.currentTarget);
+  };
+  const handleShareClose = () => setAnchorShareEl(null);
+
+  const shareLinks = {
+    telegram: `https://t.me/share/url?url=${encodeURIComponent(url)}`,
+    whatsapp: `https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`,
+    twitter: `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}`,
+    facebook: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+      url
+    )}`,
+  };
   return (
     <Stack
       className="container"
@@ -52,13 +86,120 @@ const PropertyOverview = () => {
         </Stack>
       </Stack>
       <Stack justifyContent={"space-around"}>
-        <Stack flexDirection={"row"} gap={3}>
+        <Stack flexDirection={"row"} gap={3} alignItems={"center"}>
           {like ? (
             <FavoriteIcon sx={{ color: "red", fontSize: 29 }} />
           ) : (
             <FavoriteBorderIcon sx={{ color: "red", fontSize: 29 }} />
           )}
-          <ShareIcon />
+          <Button onClick={handleShareClick}>
+            <ShareIcon />
+          </Button>
+          <Menu
+            anchorEl={anchorShareEl}
+            open={shareOpen}
+            onClose={handleShareClose}
+            slotProps={{
+              paper: {
+                sx: {
+                  border: "1px solid #ccc",
+                  boxShadow: "0 8px 20px rgba(0, 0, 0, 0.15)",
+                  borderRadius: 2,
+                  mt: 1,
+                  p: 1,
+                },
+              },
+            }}
+          >
+            <MenuItem
+              onClick={handleShare}
+              sx={{ borderBottom: "1px solid", borderColor: "primary" }}
+            >
+              <Stack flexDirection={"row"} gap={1} alignItems={"center"}>
+                <Typography fontWeight={800}>Copy Link</Typography>{" "}
+                <ContentCopyIcon sx={{ fontSize: "15px" }} />
+              </Stack>
+            </MenuItem>
+            <MenuItem onClick={handleShareClose}>
+              <a
+                href={shareLinks.telegram}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Stack
+                  flexDirection={"row"}
+                  gap={1}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                >
+                  <Typography fontWeight={800} color={"primary"}>
+                    {" "}
+                    Telegram
+                  </Typography>{" "}
+                  <TelegramIcon sx={{ fontSize: "19px" }} />
+                </Stack>
+              </a>
+            </MenuItem>
+            <MenuItem onClick={handleShareClose}>
+              <a
+                href={shareLinks.whatsapp}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Stack
+                  flexDirection={"row"}
+                  gap={1}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                >
+                  <Typography fontWeight={800} color={"primary"}>
+                    {" "}
+                    WhatsApp
+                  </Typography>{" "}
+                  <WhatsAppIcon sx={{ fontSize: "19px" }} />
+                </Stack>
+              </a>
+            </MenuItem>
+            <MenuItem onClick={handleShareClose}>
+              <a
+                href={shareLinks.twitter}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Stack
+                  flexDirection={"row"}
+                  gap={1}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                >
+                  <Typography fontWeight={800} color={"primary"}>
+                    Twitter
+                  </Typography>{" "}
+                  <TwitterIcon sx={{ fontSize: "19px" }} />
+                </Stack>
+              </a>
+            </MenuItem>
+            <MenuItem onClick={handleShareClose}>
+              <a
+                href={shareLinks.facebook}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Stack
+                  flexDirection={"row"}
+                  gap={1}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                >
+                  <Typography fontWeight={800} color={"primary"}>
+                    {" "}
+                    Facebook
+                  </Typography>{" "}
+                  <FacebookIcon sx={{ fontSize: "19px" }} />
+                </Stack>
+              </a>
+            </MenuItem>
+          </Menu>
         </Stack>
         <Stack flexDirection={"row"}>
           <Button
