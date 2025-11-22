11a7101043f485678ce7810179871ee118c60990
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-11-22 20:50:35 +0900

    fix the map issue

--- src/libs/components/HotelDetail.tsx/CustomMap.tsx
+++ src/libs/components/HotelDetail.tsx/CustomMap.tsx
@@ -1,34 +1,5 @@
-import { Stack } from "@mui/material";
-import React, { ComponentType } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import { renderToString } from "react-dom/server";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import FmdGoodIcon from "@mui/icons-material/FmdGood";
-
-// Function to create icon from MUI component
-const createIconFromMuiComponent = (color: string = "#1976d2") => {
-  const iconHtml = renderToString(
-    <FmdGoodIcon style={{ fontSize: 40, color }} />
-  );
-
-  return L.divIcon({
-    html: iconHtml,
-    className: "custom-marker-icon",
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -40],
-  });
-};
-
-// Custom marker icon
-const getMarkerIcon = (country: string) => {
-  if (country === "South Korea") {
-    return createIconFromMuiComponent("#dc2626");
-  }
-  return createIconFromMuiComponent("#1976d2");
-};
+import React from "react";
+import dynamic from "next/dynamic";
 
 interface MapProps {
   country?: string;
@@ -36,23 +7,64 @@ interface MapProps {
   lng: number;
 }
 
+// This is just a type-safe wrapper, the actual component is loaded dynamically below
 const CustomMap = ({ country, lat, lng }: MapProps) => {
-  return (
-    <MapContainer
-      center={[lat, lng]}
-      zoom={15}
-      style={{ height: 200, width: 390 }}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[lat, lng]} icon={getMarkerIcon(country || "")}>
-        <Popup>{country}</Popup>
-      </Marker>
-    </MapContainer>
-  );
+  return null; // This never actually renders
 };
 
-export default CustomMap;
+export default dynamic<MapProps>(
+  () =>
+    import("react-leaflet").then((mod) => {
+      const { MapContainer, TileLayer, Marker, Popup } = mod;
+      const L = require("leaflet");
+      require("leaflet/dist/leaflet.css");
+      const { renderToString } = require("react-dom/server");
+      const FmdGoodIcon = require("@mui/icons-material/FmdGood").default;
+
+      // Function to create icon from MUI component
+      const createIconFromMuiComponent = (color: string = "#1976d2") => {
+        const iconHtml = renderToString(
+          React.createElement(FmdGoodIcon, { style: { fontSize: 40, color } })
+        );
+
+        return L.divIcon({
+          html: iconHtml,
+          className: "custom-marker-icon",
+          iconSize: [40, 40],
+          iconAnchor: [20, 40],
+          popupAnchor: [0, -40],
+        });
+      };
+
+      // Custom marker icon
+      const getMarkerIcon = (country: string) => {
+        if (country === "South Korea") {
+          return createIconFromMuiComponent("#dc2626");
+        }
+        return createIconFromMuiComponent("#1976d2");
+      };
+
+      return ({ country, lat, lng }: MapProps) => (
+        <MapContainer
+          center={[lat, lng]}
+          zoom={15}
+          style={{ height: 200, width: 390 }}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[lat, lng]} icon={getMarkerIcon(country || "")}>
+            <Popup>{country}</Popup>
+          </Marker>
+        </MapContainer>
+      );
+    }),
+  {
+    ssr: false,
+    loading: () => (
+      <div style={{ height: 200, width: 390 }}>Loading map...</div>
+    ),
+  }
+);
